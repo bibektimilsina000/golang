70f2d16dccee3fcb86982e6da9073b1b43f8af36
Bibek Timilsina <bibektimilsina000@gmail.com>
2022-01-02 16:24:51 +0545

    first commit

---
+++ variable and data type.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name string = "hello go "
+
+	var timmy string
+
+	timmy = "this is timmy variable"
+
+	fmt.Println(name)
+	fmt.Println(timmy)
+
+}
